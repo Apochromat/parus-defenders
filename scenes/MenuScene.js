8104a79603a67267a6668c17c85213bc3f29a0ab
Alexander <podsolnyx03@mail.ru>
2022-05-29 14:46:56 +0700

    new main menu

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -8,10 +8,14 @@ export class MenuScene extends Phaser.Scene{
     init() {
     }
     preload() {
+        this.load.image('backgroundMenu', "assets/images/background_menu.png");
         this.load.image('playButton', "assets/images/play_button.png");
+        this.load.image('optionButton', "assets/images/options_button.png");
     }
     create() {
-        let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "playButton").setDepth(1);
+        this.add.tileSprite(CST.NUMBERS.WIDTH/2, CST.NUMBERS.HEIGHT/2, CST.NUMBERS.WIDTH, CST.NUMBERS.HEIGHT, 'backgroundMenu');
+        let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height - 220, "playButton").setDepth(1);
+        let optionButton = this.add.image(this.game.renderer.width / 25, this.game.renderer.height - this.game.renderer.height / 12, "optionButton").setDepth(1);
         playButton.setInteractive();
 
         playButton.on("pointerup", () => {
